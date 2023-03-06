42158342b0fa7fc821a865a456aea4f4cb361e56
chitra <chmanu.chinu@gmail.com>
2023-03-06 10:45:28 +0000

    update learn-terraform

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -3,5 +3,29 @@ variable "d1" {
     
 }
 resource "null_resource" "nothing" {
-    
+    count = var.d1
+}
+
+variable "d2" {
+    default = [fruit, banana, 2, 3, 4]
+}
+
+resource "null_resource" "d2" {
+    count = length(var.d2)
+}
+variable "d3" {
+    default = {
+         fruit = {
+            name = "fruit"
+            count = 3
+        }
+        banana = {
+            name = "banana"
+            count = 30
+        }
+    }
+}
+
+resource "null_resource" "d2" {
+    for_each = var.d3
 }
