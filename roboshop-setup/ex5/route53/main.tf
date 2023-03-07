de1941c552dcbc8472cef39724e6e359732da00f
chitra <chmanu.chinu@gmail.com>
2023-03-07 10:35:17 +0000

    update learn-terraform

---
+++ roboshop-setup/ex5/route53/main.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "record" {
+  zone_id = "Z05324103C6FB88430WB"
+  name    = "${var.component}-dev.learndevopsb71shop.site"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
