d96cc68d01bc41e15141b87fbc83f6bdf751feec
chitra <chmanu.chinu@gmail.com>
2023-03-07 09:42:35 +0000

    update learn-terraform

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
     ami                   = data.aws_ami.ami.image_id
     instance_type         = var.instance_type
-    vpc_security_group_ids =  ["sg-00710f89f3810d79e"]
+    vpc_security_group_ids =  [var.sg_id]
     tags = {
         Name = var.component
     }
