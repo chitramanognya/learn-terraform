438308e129b999b20197ec2c3530766a8ac4eb6a
chitra <chmanu.chinu@gmail.com>
2023-03-07 11:35:04 +0000

    update learn-terraform

--- roboshop-setup/ex5/route53/main.tf
+++ roboshop-setup/ex5/route53/main.tf
@@ -3,6 +3,7 @@ resource "aws_route53_record" "record" {
   name    = "${var.component}-dev.learndevopsb71shop.site"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.ec2.private_ip]
+  records = [var.private_ip]
 }
 variable "private_ip" {}
+variable "component" {}
