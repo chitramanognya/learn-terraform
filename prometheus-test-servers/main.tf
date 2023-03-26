5e37217dafd0d19d9146ac428d73359cb6b8ef68
chitra <chmanu.chinu@gmail.com>
2023-03-26 03:35:36 +0000

    update learn-terraform

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0ed0a319e81191f30"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-00710f89f3810d79e"]
-  wait_for_fulltime = true
+  wait_for_fulfillment = true
 
   tags = {
     Name = "prom-test-server"
